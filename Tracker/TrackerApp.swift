d5e40356d99d0b73ac5d8139a476190523d719b1
Monu Chaudhary <chaudharymonu45@gmail.com>
2022-07-31 15:19:45 +0530

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Monu Chaudhary on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
